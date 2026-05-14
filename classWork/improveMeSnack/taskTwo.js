e7a9f10300956fd6df0b0b052706262533611642
Isaac Urban <kpomassiurban@gmail.com>
2026-05-14 23:41:31 +0100

    done with task 2

---
+++ classWork/improveMeSnack/taskTwo.js
@@ -0,0 +1,14 @@
+function age(age){
+    if (age > 18){
+        console.log("You are an adult");
+    }
+    else if (age >= 13 && age <= 18){
+        console.log("You are a teen");
+    }
+    else{   
+        console.log("You are an adult")
+    } 
+}
+
+
+age(15);
